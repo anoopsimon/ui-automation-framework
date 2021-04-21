a4e96a442cabcedb2d16a289667e779a5de14e23
Anoop Simon <s451421990@gmail.com>
2021-04-21 10:59:21 +1000

    script fix

--- src/Automation.UI.Tests/Steps/CoreSteps.cs
+++ src/Automation.UI.Tests/Steps/CoreSteps.cs
@@ -2,30 +2,26 @@
 using Automation.Core.Enums;
 using Automation.Core.Web;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using System;
 using TechTalk.SpecFlow;
-
 namespace Automation.UI.Tests.Steps
 {
     [Binding]
     public class CoreSteps
     {
         public readonly AppSettings AppSettings;
-
         public CoreSteps()
         {
             AppSettings = new AppSettings();
         }
+
         [StepDefinition(@"I launch '(.*)' browser")]
         public void GivenILaunchBrowser(string browserName)
-        {      
-
-           // var session = new SessionManager(false,"",Browser.CHROME);
+        {     
+            //var session = new SessionManager(false,"",Browser.CHROME);
             var session = new SessionManager(true,"http://localhost:4444/wd/hub",Browser.CHROME);
-           var driver= session.CreateSession();
+            var driver= session.CreateSession();
             driver.Url = "https://www.bing.com/";
-//            var address = session.GetDevToolSesion().EndpointAddress;
+            //var address = session.GetDevToolSesion().EndpointAddress;
             //Console.WriteLine("address > " + address);
             var commands = new Commands(driver);
             commands.Type(By.CssSelector("#sb_form_q"),"automation");
